d1764397e52de51b4fb43a3039df145f4c3e59ef
Manuel Fernández <manueleloyfernandez@gmail.com>
2022-08-28 20:23:58 -0300

    lista terminada

--- index.js
+++ index.js
@@ -1,77 +1,91 @@
 const input = document.querySelector(".input-texto");
 const formAñadir = document.querySelector(".form-añadir");
-
-const listaTareas = document.querySelector (".lista-tareas");
+const listaTareas = document.querySelector(".lista-tareas");
+const botonBorrarTodo = document.querySelector(".borrar-todo");
 
 let tareas = JSON.parse(localStorage.getItem("tareass")) || [];
 
 const guardarEnLocalStorage = (tareasVarias) => {
-
-    console.log(tareas);
-    localStorage.setItem("tareass", JSON.stringify(tareasVarias));
+  
+  localStorage.setItem("tareass", JSON.stringify(tareasVarias));
 };
 
 let id = 0;
 
 
-const pintarLista = (lista) => {
+const ocultarBorrarTodo = (tareasLista) => {
 
-    listaTareas.innerHTML = lista.map((tarea) => `<li>${tarea.name}<i class="fa-solid fa-trash-can"></i></li>`).join("");
+    if (!tareas.length) {
 
-}
-
-
-const añadirTarea = (e) => {
-
-    e.preventDefault();
-    const nombreTarea = input.value.trim();
-
-    if (!nombreTarea.length) {
-
-        console.log("No escribiste nada, bro");
-        alert("Escribite algo");
-    }
-
-    else if (tareas.some((task) => task.name.toUpperCase() === nombreTarea.toUpperCase())){
-
-        alert("Ya existe esa tarea");
-      
-    }
-
-    else {
-
-        tareas = [...tareas, {name: nombreTarea, idTarea: id + 1}];
-        console.log(tareas);
-        guardarEnLocalStorage(tareas);
-        input.value = "";
-        id++;
-        pintarLista(tareas);
-        
-    }
-  
-};
-
-const borrarTareas = () => {
-
-    if (!e.target.classList.contains("boton-borrar")) {
-
+        botonBorrarTodo.classList.add("oculto");
         return;
     }
-
-    else  {
-
-        
-    }
-    
-
+    else {
+    botonBorrarTodo.classList.remove("oculto");
 
 }
+}
 
+
+const pintarLista = (lista) => {
+  listaTareas.innerHTML = lista
+    .map(
+      (tarea) => `<li>${tarea.name}<i class="fa-solid fa-trash-can" data-id=${tarea.idTarea}></i></li>`
+    )
+    .join("");
+};
+
+const añadirTarea = (e) => {
+  e.preventDefault();
+  const nombreTarea = input.value.trim();
+
+  if (!nombreTarea.length) {
+    console.log("No escribiste nada, bro");
+    alert("Escribite algo");
+  } else if (
+    tareas.some((task) => task.name.toUpperCase() === nombreTarea.toUpperCase())
+  ) {
+    alert("Ya existe esa tarea");
+    return;
+  } else {
+    tareas = [...tareas, { name: nombreTarea, idTarea: tareas.length + 1}];
+    console.log(tareas);
+    guardarEnLocalStorage(tareas);
+    
+    pintarLista(tareas);
+    ocultarBorrarTodo(tareas)
+  }
+};
+
+const borrarTareas = (e) => {
+  if (!e.target.classList.contains("fa-trash-can")) {
+    console.log("hola");  
+    return;
+     
+  } 
+  console.log("chau");
+  
+  const id = +e.target.dataset.id;
+
+  tareas = tareas.filter((tarea) => tarea.idTarea !== id);
+  guardarEnLocalStorage(tareas);
+  pintarLista(tareas);
+  ocultarBorrarTodo(tareas)
+};
+
+const borrarTodo = () => {
+
+    tareas = [];
+    guardarEnLocalStorage(tareas);
+    pintarLista(tareas);
+    ocultarBorrarTodo(tareas)
+}
 const iniciar = () => {
-
-formAñadir.addEventListener("submit", añadirTarea);
-pintarLista(tareas);
-listaTareas.addEventListener("click", borrarTareas);
+  formAñadir.addEventListener("submit", añadirTarea);
+  pintarLista(tareas);
+  listaTareas.addEventListener("click", borrarTareas);
+  botonBorrarTodo.addEventListener("click", borrarTodo);
+  ocultarBorrarTodo(tareas);
 };
 
 iniciar();
